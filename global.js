3006f18045d9172dfbc796ed41f60b7c8725cffe
Abderrahmane_Ahouari <abdorrahmane054@gmail.com>
2024-10-31 18:48:24 +0100

    i finished working on the interface and i worked a little bit in the back end and now i am gonna complete working on it

--- global.js
+++ global.js
@@ -1,1 +1,67 @@
-document.getElementById("demo").style.display = "none";
+let task = document.getElementById("task");
+let form = document.getElementById("form");
+let close_btn = document.getElementById("btn_close");
+
+task.addEventListener("click",()=>{
+    form.classList.remove("hidden");
+})
+
+
+
+
+close_btn.addEventListener("click",()=>{
+    form.classList.add("hidden");
+})
+
+
+
+let tite = document.getElementById("titre");
+let description = document.getElementById("description");
+let datestart = document.getElementById("datestart");
+let dateend = document.getElementById("dateend");
+let category = document.getElementById("category")
+let statu = document.getElementById("statu"); 
+let addtask = document.getElementById("addtask"); 
+
+
+
+let tasks
+if(localStorage.tasksStock != null){
+    tasks = JSON.parse(localStorage.tasksStock)
+}else{
+     tasks = [];
+}
+
+addtask.onclick = function () {   
+    form.classList.add("hidden");
+     let newtask = {
+        id:Math.random()*100,
+            title : tite.value,
+            description : description.value, 
+            datestart : datestart.value, 
+            dateend : dateend.value, 
+            category : category.value, 
+            statu : statu.value, 
+ 
+     }
+     tasks.push(newtask);
+     localStorage.setItem('tasksStock', JSON.stringify(tasks))   
+     clearinputs()
+}
+
+
+function clearinputs(){
+      tite.value = ''
+      description.value = ''
+      dateend.value = ''
+      datestart.value = ''
+      category.value = 'P1'
+      statu.value = 'To do'
+}
+
+
+
+
+
+
+ 
